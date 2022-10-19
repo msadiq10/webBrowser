276937cbec3b4cf7f32473a7a3fdea2e6894eb56
Mohammed Sadiq Bagalkot <mb200@hw.ac.uk>
2022-10-19 21:31:52 +0400

    Home and reload buttons made functional

---
+++ WebBrowserApp/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace WebBrowserApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.homeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(569, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Status:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.Location = new System.Drawing.Point(10, 76);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1148, 498);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(221, 9);
+            this.searchBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(617, 23);
+            this.searchBox.TabIndex = 4;
+            this.searchBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.searchBox_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 9);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 22);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Reload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.reload);
+            // 
+            // homeButton
+            // 
+            this.homeButton.Location = new System.Drawing.Point(109, 9);
+            this.homeButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.homeButton.Name = "homeButton";
+            this.homeButton.Size = new System.Drawing.Size(82, 22);
+            this.homeButton.TabIndex = 6;
+            this.homeButton.Text = "Home";
+            this.homeButton.UseVisualStyleBackColor = true;
+            this.homeButton.Click += new System.EventHandler(this.homeButton_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1169, 582);
+            this.Controls.Add(this.homeButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button homeButton;
+    }
+}
+
---
+++ WebBrowserApp/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowserApp
+{
+    public partial class Form1 : Form
+    {
+        string url = "https://www.google.com/"; // homepage
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            home();
+        }
+
+        private async Task home()
+        {
+            using var client = new HttpClient();
+
+            searchBox.Text = url;
+
+            string urlNotFound = "http://www.macs.hw.ac.uk/~hwloidl/Courses/F21SC/httpstat.us/404";
+
+            var result = await client.GetAsync(url);
+
+            richTextBox1.Text = "Loading...";
+
+            if (result.StatusCode.ToString().Equals("BadRequest"))
+            {
+                label1.Text = "Error: Bad Request!";
+                richTextBox1.Text = "";
+            }
+            else if (result.StatusCode.ToString().Equals("Forbidden"))
+            {
+                label1.Text = "Error: Forbidden!";
+                richTextBox1.Text = "";
+            }
+            else if (result.StatusCode.ToString().Equals("NotFound"))
+            {
+                label1.Text = "Error: Not found!";
+                richTextBox1.Font = new Font("Segoe UI", 9, FontStyle.Bold);
+                richTextBox1.Text = "ERROR 404: NOT FOUND";
+            }
+            else
+            {
+                label1.Text = "Status: "+result.StatusCode.ToString();
+                richTextBox1.Font = new Font("Segoe UI", 9, FontStyle.Regular);
+                var content = await client.GetStringAsync(url);
+
+                richTextBox1.Text = content;
+            }
+        }
+
+        private void reload(object sender, EventArgs e)
+        {
+            url = searchBox.Text;
+            home();
+        }
+
+        private void searchBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                url = searchBox.Text;
+                home();
+            }
+        }
+
+        private void homeButton_Click(object sender, EventArgs e)
+        {
+            url = "https://www.google.com/";
+            home();
+        }
+
+        private void homeButton_Click_1(object sender, EventArgs e)
+        {
+            url = "https://www.google.com/";
+            home();
+        }
+    }
+}
